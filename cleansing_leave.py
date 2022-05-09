416ccbf523dfe36612073dc162d3724260d0676a
Leng Thodsapon <82572605+lengtsp@users.noreply.github.com>
2022-05-09 13:39:45 +0700

    Create cleansing_leave.py

---
+++ cleansing_leave.py
@@ -0,0 +1,50 @@
+def convert_leavetype(ty):
+    if ty == 'ANL1':
+        return 8
+    elif ty == 'ANL2':
+        return 8
+    elif ty == 'ANLCARR':
+        return 34
+    elif ty == 'HJL1':
+        return 13
+    elif ty == 'HJL2':
+        return 13
+    elif ty == 'MSL':
+        return 10
+    elif ty == 'MTL_1':
+        return 4
+    elif ty == 'MTL_2':
+        return 33
+    elif ty == 'MTL_3':
+        return 5
+
+    elif ty == 'MTL_INF1':
+        return 4
+    elif ty == 'MTL_INF2':
+        return 33
+    elif ty == 'OLP1.1':
+        return 6
+    elif ty == 'OLP1.2':
+        return 6
+    elif ty == 'OLP2':
+        return 6
+
+    elif ty == 'PTL':
+        return 31
+    elif ty == 'PVL':
+        return 2
+    elif ty == 'SL':
+        return 3
+    elif ty == 'TNL':
+        return 12
+
+    elif ty == 'VPSNL':
+        return 30
+  
+    elif ty == 'STL':
+        return 19
+
+
+    else:
+        return 'N/A'
+
